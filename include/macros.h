72df01e51b1dea9747f39be49e4178a9dc33b694
Robert Krantz <krantzr@hotmail.se>
2024-04-04 12:50:21 +0200

    Add files via upload

--- include/macros.h
+++ include/macros.h
@@ -17,6 +17,7 @@
 #define LOW 0
 
 #define F_CPU 16000000UL
+#define TIMER_FREQUENCY 1000UL
 #define BAUD 9600
 #define UBRR_VALUE (F_CPU / (16UL * BAUD)) - 1
 #define UART_DATA_REG      (*(volatile uint8_t *)0xC6)
@@ -25,6 +26,7 @@
 #define UART_CONTROL_REG_B (*(volatile uint8_t *)0xC1)
 #define UART_CONTROL_REG_A (*(volatile uint8_t *)0xC0)
 #define UART_CONTROL_REG_C (*(volatile uint8_t *)0xC2)
+#define SREG_I 7
 #define RXCIE0 7
 #define TXCIE0 6
 #define UDRIE0 5
@@ -36,5 +38,17 @@
 #define RXC0   7
 #define UDRE0  5
 #define UCSZ00 1
+
+#define STATUS_REGISTER    (*(volatile uint8_t *)0x5F)
+#define TCCR1B_REG         (*(volatile uint8_t *)0x81)
+#define OCR1A_REGH         (*(volatile uint8_t *)0x89)
+#define OCR1A_REGL         (*(volatile uint8_t *)0x88)
+#define TIMSK1_REG         (*(volatile uint8_t *)0x6F)
+#define WGM12_BIT    3
+#define CS11_BIT     1
+#define CS10_BIT     0
+#define OCIE1A_BIT   1
+
+
 
 #endif  // MACROS_H
