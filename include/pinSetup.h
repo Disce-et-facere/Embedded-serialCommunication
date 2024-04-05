3dacf43205724c07fcf3780f776c56e728e8937b
Robert Krantz <krantzr@hotmail.se>
2024-04-05 23:20:22 +0200

    Delete include directory

--- include/pinSetup.h
+++
@@ -1,7 +0,0 @@
-#ifndef PINSETUP_H
-#define PINSETUP_H
-
-void pinMode(volatile uint8_t *dataDirection, uint8_t pin, uint8_t state);
-void pinInit();
-
-#endif  // PINSETUP_H
--- include/readWritePin.h
+++
@@ -1,9 +0,0 @@
-#ifndef READWRITEPIN_H
-#define READWRITEPIN_H
-
-uint8_t digitalRead(uint8_t pinReg, uint8_t pin);
-void togglePinBit(volatile uint8_t *portReg, uint8_t pin);
-void setPinBit(volatile uint8_t *portReg, uint8_t pin);
-void clearPinBit(volatile uint8_t *portReg, uint8_t pin);
-
-#endif  // READWRITEPIN_H
