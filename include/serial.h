1b206312aa976ac1553670b9daa86951d68170e1
Robert Krantz <krantzr@hotmail.se>
2024-04-15 11:26:50 +0200

    Update serial.h

--- include/serial.h
+++ include/serial.h
@@ -6,7 +6,8 @@ uint8_t serialAvaliable();
 void serialWrite(uint8_t data);
 uint8_t serialRead();
 void serialWriteString(const char* str);
-char* serialReadString();
+void serialWriteLine(const char* str);
+void serialReadString(char* destination);
 void serialParser();
 
 #endif  // SERIAL_H
